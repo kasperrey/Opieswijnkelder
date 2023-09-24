56b796b423656a960ca8de573bda304e32035d8a
kasperrey <kasper.reynders@gmail.com>
2023-09-24 08:27:54 +0200

    eerste versie

--- app/src/main/java/com/example/opieswijnkelder/MainActivity.java
+++ app/src/main/java/com/example/opieswijnkelder/MainActivity.java
@@ -138,7 +138,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void createNotification(String title, String text) {
         NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "overdatum")
-                .setSmallIcon(R.mipmap.)
+                .setSmallIcon(R.mipmap.ic_launcher)
                 .setContentTitle(title)
                 .setContentText(text)
                 .setPriority(NotificationCompat.PRIORITY_LOW);
